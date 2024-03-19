b7b289a9defcffa11e52b67972281bd62deeca3b
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-03-19 16:03:31 +0900

    feat: global style 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import TodoListPage from "./pages/todoList/todoList";
+import { GlobalStyle } from "./styles/GlobalStyles";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <TodoListPage />
     </div>
   );
