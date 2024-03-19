82039db2308d443c4f30543f4351577baac7ebad
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-03-19 16:03:50 +0900

    feat: todoList UI 구성

--- src/pages/todoList/todoList.js
+++ src/pages/todoList/todoList.js
@@ -1,3 +1,47 @@
+import { useState } from "react";
+import * as T from "./todoList.styles";
+
 export default function TodoListPage() {
-  return <div>123</div>;
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const onChangeTodo = (event) => {
+    setTodo(event.currentTarget.value);
+  };
+
+  const onClickSubmit = () => {
+    if (!todo.trim()) return;
+    setTodoList([...todoList, todo]);
+    // todo 상태 초기화
+    setTodo("");
+  };
+
+  return (
+    <T.Wrapper>
+      <T.Header>✓ To Do</T.Header>
+      <T.TodoContainer>
+        <T.Title>오늘 할 일</T.Title>
+        <T.TodayDate></T.TodayDate>
+
+        {/*  input container  */}
+        <T.WriteForm>
+          <T.TodoForm>
+            <T.CheckBoxImg />
+            <T.TodoInput type="text" onChange={onChangeTodo} />
+            <T.SubmitBtn onClick={onClickSubmit}>추가</T.SubmitBtn>
+          </T.TodoForm>
+        </T.WriteForm>
+
+        {/*  todo list  */}
+        <T.TodoList>
+          {todoList?.map((item, index) => (
+            <T.TodoListLi key={index}>
+              <T.TodoListSpan>{item}</T.TodoListSpan>
+              <T.TodoListBtn type="button" />
+            </T.TodoListLi>
+          ))}
+        </T.TodoList>
+      </T.TodoContainer>
+    </T.Wrapper>
+  );
 }
---
+++ src/pages/todoList/todoList.styles.js
@@ -0,0 +1,134 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+`;
+
+export const Header = styled.div`
+  width: 100%;
+  height: 6vh;
+  background-color: #1b1a19;
+
+  color: #e9ecef;
+  text-align: left;
+  padding-left: 45px;
+  line-height: 6vh;
+  font-size: 1.1rem;
+  border-bottom: 1px solid #484644;
+`;
+
+export const TodoContainer = styled.div`
+  width: 50%;
+  min-height: 95vh;
+  background-color: #1b1a19;
+  padding: 30px;
+  border-radius: 5px;
+  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.3);
+`;
+
+export const Title = styled.h1``;
+
+export const TodayDate = styled.p`
+  font-size: 0.625rem;
+  color: gray;
+  margin: 6px 0 0 1px;
+`;
+
+export const WriteForm = styled.div`
+  width: 100%;
+  border-radius: 10px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  background-color: #252423;
+`;
+
+export const TodoForm = styled.form`
+  width: 100%;
+  height: 52px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0 15px 0 15px;
+`;
+
+export const CheckBoxImg = styled.img`
+  width: 20px;
+  height: 20px;
+  margin: 0 15px 0 0px;
+  fill: #788bff;
+`;
+
+export const TodoInput = styled.input`
+  width: 100%;
+  height: 50px;
+  background-color: #252423;
+  outline: none;
+  border: none;
+  color: #e9ecef;
+  padding: 10px;
+  ::placeholder {
+    color: #788bff;
+  }
+`;
+
+export const SubmitBtn = styled.button`
+  cursor: pointer;
+  background-color: #252423;
+  border: 1px solid #788bff;
+  width: 36px;
+  height: 22px;
+  border-radius: 10px;
+  font-size: 0.75rem;
+  color: #788bff;
+  :hover {
+    background-color: #3c3a39;
+  }
+`;
+
+export const TodoList = styled.div`
+  width: 100%;
+  list-style: none;
+`;
+
+export const TodoListLi = styled.li`
+  width: 100%;
+  height: 52px;
+  border-radius: 10px;
+  font-size: 0.81rem;
+
+  display: flex;
+  justify-content: space-between;
+
+  align-items: center;
+  background-color: #252423;
+  padding-right: 5px;
+  margin: 5px 0px;
+  padding: 0 15px 0 15px;
+  color: #cdcdcd;
+`;
+export const TodoListSpan = styled.span`
+  width: 100%;
+  padding: 0px 26.1px;
+  font-size: 0.73rem;
+`;
+
+export const TodoListBtn = styled.button`
+  cursor: pointer;
+  background-color: #252423;
+  border: 1px solid #788bff;
+  width: 36px;
+  height: 22px;
+  border-radius: 10px;
+  font-size: 0.75rem;
+  color: #788bff;
+
+  :hover {
+    background-color: #3c3a39;
+  }
+`;
