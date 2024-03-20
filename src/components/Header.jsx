5fa2abda9d9a88f6e5ad00845112213b2ca817d1
CSE-pebble <CSE.pebble@gmail.com>
2024-03-21 00:55:14 +0900

    Feat: 오늘 날짜를 보여주는 기능 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
 import styled from "styled-components";
 
 const Header = () => {
+  const getTodayDate = () => {
+    const today = new Date();
+    const week = ["일", "월", "화", "수", "목", "금", "토"];
+
+    const year = today.getFullYear();
+    const month = ("0" + (today.getMonth() + 1)).slice(-2);
+    const day = ("0" + today.getDate()).slice(-2);
+    const dayOfWeek = week[today.getDay()];
+
+    return `${year}.${month}.${day} ${dayOfWeek}요일`;
+  };
+
   return (
     <Wrapper>
       <Title>아기자기 TODO</Title>
-      <Date>날짜</Date>
+      <TodayDate>{getTodayDate()}</TodayDate>
     </Wrapper>
   );
 };
@@ -23,6 +35,6 @@ const Title = styled.h2`
   font-size: 30px;
 `;
 
-const Date = styled.span`
+const TodayDate = styled.span`
   font-size: 25px;
 `;
